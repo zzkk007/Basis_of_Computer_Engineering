00e4ca46cb68718d3befadd92810906b185cceea
zhangkun <zhangkun09211916@gmail.com>
2018-12-21 09:34:55 +0800

    Basic math course for program

--- code_test/Basic_math_course_Lesson5_1.py
+++ code_test/Basic_math_course_Lesson5_1.py
@@ -1,35 +1,21 @@
+import copy
+def get_items(totalReward, result):
 
-class Lesson5_1(object):
+    rewards = [1, 2, 5, 10]
 
-    # 四种面额的纸币
-    def __init__(self):
-        self.rewards = [1, 2, 5, 10]
-
-    """
-        :description 使用递归调用，找出所有可能的奖赏组合
-        :param totalReward 奖赏总金额，result 保存当前的解
-        : return void
-    """
-    def get(self, totalReward, result):
-
-        #当 totalReward = 0 时，证明它是满足条件的解，结束嵌套，输出解
-        if totalReward == 0:
-            print(result)
-            return
-        # 当 totalReword < 0 时，证明它不满足条件的解，不输出。
-        elif totalReward < 0:
-            return
-        else:
-            for i in range(len(self.rewards)):
-                newReword = []
-                newReword.append(self.rewards[i])
-                self.get(totalReward - self.rewards[i], newReword)
+    if totalReward == 0:
+        print(result)
+        return
+    elif totalReward < 0:
+        return
+    else:
+        for i in range(len(rewards)):
+            newReword = copy.deepcopy(result)
+            newReword.append(rewards[i])
+            get_items(totalReward - rewards[i], newReword)
 
 if __name__ == "__main__":
 
-    totalReword = 10
     alist = list()
-    lesson = Lesson5_1()
-    lesson.get(totalReword, alist)
+    get_items(10, alist)
 
-    print(alist)
