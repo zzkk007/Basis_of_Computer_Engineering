876c2546594d5c454e5609f34e1be9595500995c
zhangkun <zhangkun09211916@gmail.com>
2019-01-03 11:47:30 +0800

    Algorithm for the interview

--- code_test/Basic_math_course_Lesson8_1.py
+++ code_test/Basic_math_course_Lesson8_1.py
@@ -1,4 +1,4 @@
-
+import copy
 def Lesson8_1(teams, result, m):
     """
     :description: 使用函数的递归调用，找出所有可能的队列组合。
@@ -14,3 +14,16 @@ def Lesson8_1(teams, result, m):
         return
 
     for i in range(len(teams)):
+        # 从剩下的队伍中，选择一队，加入结果
+        new_result = copy.deepcopy(result)
+        new_result.append(teams[i])
+
+        # 只考虑当前选择之后的所有队伍
+        rest_teams = copy.deepcopy(teams[i+1:])
+
+        # 递归调用，对于剩余的队伍继续生成组合
+        Lesson8_1(rest_teams, new_result, m)
+
+if __name__ == "__main__":
+    teams = ['t1', 't2', 't3', 't4']
+    Lesson8_1(teams, [], 2)
