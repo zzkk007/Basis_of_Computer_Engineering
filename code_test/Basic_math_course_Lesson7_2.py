dfd09786dc5ae4b5156d43d946e3b3840d2f4974
zhangkun <zhangkun09211916@gmail.com>
2019-01-03 09:46:33 +0800

    Algorithm for the interview

--- code_test/Basic_math_course_Lesson7_2.py
+++ code_test/Basic_math_course_Lesson7_2.py
@@ -4,18 +4,20 @@ classes = ['a', 'b', 'c', 'd', 'e']
 password = 'bacd'
 
 
-def get_passwor(n, result = ''):
+def get_password(n, result = ''):
     if n == 0:
         if result == password:
-            print(password)
+            print(result)
+        else:
+            print(result)
     else:
 
         for i in classes:
             new_result = copy.deepcopy(result)
             new_result = new_result + i
-            get_passwor(n - 1, new_result)
+            get_password(n - 1, new_result)
 
 
 if __name__ == "__main__":
 
-    get_passwor(4)
+    get_password(4)
