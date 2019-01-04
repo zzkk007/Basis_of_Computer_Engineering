943ec58ee3407ee329975a114ce34f29cbdf67c9
zhangkun <zhangkun09211916@gmail.com>
2019-01-04 18:22:06 +0800

    Algorithm for the interview

--- code_test/Algorthm_for_the_interview_Lesson16_1.py
+++ code_test/Algorthm_for_the_interview_Lesson16_1.py
@@ -15,11 +15,10 @@ class Solution2(object):
     def threeSum(self, nums):
         if len(nums) < 3:
             return []
-
         nums.sort()
+
         res = set()
         for i, v in enumerate(nums[:-2]):
-            # 去掉自己本身
             if i >= 1 and v == nums[i - 1]:
                 continue
 
@@ -29,10 +28,40 @@ class Solution2(object):
                     d[-v-x] = 1
                 else:
                     res.add((v, -v-x, x))
+
         return map(list, res)
 
+
+
+class Solution3(object):
+
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i],nums[l], nums[r]))
+                    # 相连两个重复的不需要再进行判断
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+
+        return res
 
 if __name__ == "__main__":
     twoS = Solution2()
     list1 = [-1, 0, 1, 2, -1, -4]
-    print(twoS.threeSum(list1))
+    print(list(twoS.threeSum(list1)))
