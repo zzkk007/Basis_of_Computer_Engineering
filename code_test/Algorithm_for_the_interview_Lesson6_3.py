8e5a949c8f25df362c17af58ec55df82a7a86913
zhangkun <zhangkun09211916@gmail.com>
2019-01-14 11:46:15 +0800

    Basic math course for programmers

---
+++ code_test/Algorithm_for_the_interview_Lesson6_3.py
@@ -0,0 +1,74 @@
+'''
+     判断一个链表，判断是否有环
+
+'''
+
+class SingleNode(object):
+    def __init__(self, vaule):
+        self.value = vaule
+        self.next = None
+
+class SingleLinkList(object):
+    """
+        description : 一个单链表
+    """
+    def __init__(self):
+        self.head = None
+
+    def SingleAdd(self, vaule):
+
+        node = SingleNode(vaule)
+
+        if self.head is None:
+            self.head = node
+        else:
+            cur = self.head
+            while cur.next is not None:
+                cur = cur.next
+            cur.next = node
+
+    def SinglePrint(self,cur):
+        if cur is None:
+            cur = self.head
+
+        while cur is not None:
+            print('{0}'.format(cur.value))
+            cur = cur.next
+
+    def reverseList(self):
+        cur, prev = self.head, None
+        while cur is not None:
+            # python 中多变量赋值，先算好等会右边的所有值，
+            # 然后一次性赋值给左边。
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
+
+    def swapPairs(self):
+        pre, pre.next = self, self.head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+
+            pre.next, b.next, a.next = b, a, b.next
+            pre = a
+        return self.next
+
+
+if __name__ == "__main__":
+
+    listNode = [1, 2, 3, 4, 5]
+
+    slist = SingleLinkList()
+    for i in listNode:
+        slist.SingleAdd(i)
+
+    slist.SinglePrint(None)
+
+    print('---------')
+    #blist = slist.reverseList()
+    blist = slist.swapPairs()
+    slist.SinglePrint(blist)
+
+
+
+
